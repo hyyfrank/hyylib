27240bcc2079dbae672770822018a7a7eb038651
hyy <Frank.Huang@fwd.com>
2023-12-12 22:00:28 +0800

    add storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,6 @@
 module.exports = {
     env: { browser: true, es2020: true },
-    extends: [
-      'eslint:recommended', 
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react-hooks/recommended'
-      // 'plugin:react/recommended'
-    ],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint','react', 'prettier','react-refresh'],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
